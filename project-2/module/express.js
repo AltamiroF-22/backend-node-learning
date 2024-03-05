70c3a96bf61fb021b29547b8d363aa1863fe3c04
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-03-05 00:22:58 -0300

    front+back

--- project-2/module/express.js
+++ project-2/module/express.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const path = require("path");
 const UserModel = require("../src/models/user.model");
 
 const app = express();
-
 app.use(express.json());
 
-// GET ALL
-app.get("/users", async (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views"));
+
+// GET ONE AND SHOW
+app.get("/views/user/:id", async (req, res) => {
   try {
-    const users = await UserModel.find({});
-    res.status(200).json(users);
+    const userId = req.params.id;
+    const user = await UserModel.findById(userId);
+    res.render("singleUser", { user });
   } catch (err) {
-    return res.status(500).json({ error: err.message });
+    res.json({ error: err.message });
   }
 });
 
@@ -30,12 +36,64 @@ app.get("/user/:id", async (req, res) => {
     return res.status(500).json({ error: err.message });
   }
 });
+// MIDLEWARE DO CREATE USER
+const checkIfUserExist = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const existingUserEmail = await UserModel.findOne({ email: body.email });
+    const existingUserName = await UserModel.findOne({
+      userName: body.userName,
+    });
+
+    if (existingUserEmail) {
+      return res
+        .status(409)
+        .json({ error: "Já existe um usuário com este email!" });
+    }
+
+    if (existingUserName) {
+      return res
+        .status(409)
+        .json({ error: "Já existe um usuário com este user name!" });
+    }
+
+    next();
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
 
 // CREAT NEW USER
-app.post("/users", async (req, res) => {
+app.post("/create-user", checkIfUserExist, async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
-    res.status(201).json(user);
+    res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/create-user", async (req, res) => {
+  res.render("createUser");
+});
+
+// GET ALL AND SHOW
+app.get("/views/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.render("index", { users });
+  } catch (error) {
+    console.error("Error rendering view:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// GET ALL
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
@@ -78,6 +136,10 @@ app.delete("/user/:id", async (req, res) => {
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Rota não encontrada" });
 });
 
 const PORT = 2214;
--- project-2/src/models/user.model.js
+++ project-2/src/models/user.model.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  userName: {
+    type: String,
+    required: true,
+  },
+
   lastName: {
     type: String,
     required: true,
